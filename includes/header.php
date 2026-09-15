3a31fe383f99b42e5c3fee2277b2da00e89dafd1
musembimuasa <musembimuasa5@gmail.com>
2026-09-15 12:37:12 -0300

    updated code

--- includes/header.php
+++ includes/header.php
@@ -18,8 +18,8 @@ $user = current_user();
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=Manrope:wght@600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <script defer src="assets/js/app.js"></script>
+    <link rel="stylesheet" href="<?= e(BASE_URL) ?>assets/css/style.css">
+    <script defer src="<?= e(BASE_URL) ?>assets/js/app.js"></script>
 </head>
 <body data-csrf="<?= e(csrf_token()) ?>" data-authenticated="<?= $user ? '1' : '0' ?>">
 <div class="announcement">Free delivery on orders over KSh 5,000 <span>•</span> Farm fresh, every morning</div>
